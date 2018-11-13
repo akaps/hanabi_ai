b570c6cb1c9a267b6abec35eebc2ff41b5945740
Ari <ari.kaps@gmail.com>
2018-11-13 15:01:19 -0800

    Big ol' refactor of hanabi_moves
    There are shared actions between HanabiCardAction and
    HanabiDiscloseAction. Extract those methods
    rename validate to is_valid
    Restructure is_valid for disclosure subclasses
    Fix associated tests
    
    NOTE: was working ont his at the same time as playgame. Something
    probably breaks higher up. I have fixes for those already

--- tools/hanabi_moves.py
+++ tools/hanabi_moves.py
@@ -1,87 +1,77 @@
-class HanabiCardAction(object):
-    def __init__(self, player_id, card):
+class HanabiAction(object):
+    def __init__(self, player_id):
         self.player_id = player_id
+
+    def is_valid(self, game_info):
+        raise Exception('Cannot call superclass implementation')
+
+class HanabiCardAction(HanabiAction):
+    def __init__(self, player_id, card):
+        super(HanabiCardAction, self).__init__(player_id)
         self.card = card
+        self.action_type = ''
 
     def __str__(self):
-        return "Player {id} {action} {card}".format(id = self.player_id, action = self.action(), card = str(self.card))
-
-    @classmethod
-    def action(self):
-        raise Exception('Cannot call superclass implementation of action')
-
-    def validate(self, game_info):
-        raise Exception('Cannot call superclass implementation of validate')
+        return "Player {id} {action} {card}".format(id = self.player_id, action = self.action_type, card = str(self.card))
 
 class HanabiPlayAction(HanabiCardAction):
 
     def __init__(self, player_id, card):
         super(HanabiPlayAction, self).__init__(player_id, card)
+        self.action_type = 'played'
 
-    @classmethod
-    def parse_move(self, move):
-        pass
-
-    @classmethod
-    def action(self):
-        return "played"
-
-    def validate(self, game_info):
+    def is_valid(self, game_info):
         return True
 
 class HanabiDiscardAction(HanabiCardAction):
     def __init__(self, player_id, card):
         super(HanabiDiscardAction, self).__init__(player_id, card)
+        self.action_type = 'discarded'
 
-    @classmethod
-    def action(self):
-        return "discarded"
 
-    def validate(self, game_info):
+    def is_valid(self, game_info):
         return game_info.can_discard()
 
-class HanabiDiscloseAction(object):
-    def __init__(self, player_id, to_whom, count):
-        self.player_id = player_id
+class HanabiDiscloseAction(HanabiAction):
+    def __init__(self, player_id, to_whom):
+        super(HanabiDiscloseAction, self).__init__(player_id)
         self.to_whom = to_whom
-        self.count = count
+        self.count = 0
+        self.disclosure_type = ''
+
+    def pluralize(self, word, count):
+        if count <= 1:
+            return word
+        else:
+            return '{word}s'.format(word = word)
 
     def __str__(self):
         return "Player {id} told {whom} about {count} {disclosure} in their hand".format(
             id = self.player_id,
             whom = self.to_whom,
             count = self.count,
-            disclosure = self.disclosure())
+            disclosure = self.disclosure_type)
 
-    def disclosure(self):
-        raise Exception('Cannot call superclass implementation of validate')
+    def is_valid(self, game_info):
+        return game_info.can_disclose()
 
 class HanabiDiscloseColorAction(HanabiDiscloseAction):
-    def __init__(self, player_id, to_whom, count, color):
-        super(HanabiDiscloseColorAction, self).__init__(player_id, to_whom, count)
+    def __init__(self, player_id, to_whom, color):
+        super(HanabiDiscloseColorAction, self).__init__(player_id, to_whom)
         self.color = color
+        self.disclosure_type = self.pluralize(self.color, self.count)
 
-    def disclosure(self):
-        if self.count <= 1:
-            return self.color
-        else:
-            return "{color}s".format(color = self.color)
 
-    def validate(self, game_info):
-        return game_info.can_disclose() and
-            self.color in 'RWBGY*'
+    def is_valid(self, game_info):
+        return (super(HanabiDiscloseColorAction, self).is_valid(game_info) and
+            self.color in 'RWBGY*')
 
 class HanabiDiscloseRankAction(HanabiDiscloseAction):
-    def __init__(self, player_id, to_whom, count, rank):
-        super(HanabiDiscloseRankAction, self).__init__(player_id, to_whom, count)
+    def __init__(self, player_id, to_whom, rank):
+        super(HanabiDiscloseRankAction, self).__init__(player_id, to_whom)
         self.rank = rank
+        self.disclosure_type = self.pluralize(self.rank, self.count)
 
-    def disclosure(self):
-        if self.count <= 1:
-            return self.rank
-        else:
-            return "{rank}s".format(rank = self.rank)
-
-    def validate(self, game_info):
-        return game_info.can_disclose() and
-            self.rank in range(1,6)
+    def is_valid(self, game_info):
+        return (super(HanabiDiscloseRankAction, self).is_valid(game_info) and
+            self.rank in range(1,6))
