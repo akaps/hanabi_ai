9c363b811447f87210f16a8c85365069db77b75b
Ari Kapilivsky <ari.kaps@gmail.com>
2018-11-17 20:41:38 -0800

    Removing extraneous dictionary reference
    we are no longer using dictionaries for do_turn. Remove their reference

--- ai/hanabi_player.py
+++ ai/hanabi_player.py
@@ -5,17 +5,17 @@ class HanabiPlayer:
 
         @param self: class reference variable
         @param player_index: referring index for the player the AI is playing as. Locate player info using the index
-        @param game_info: dictionary of all relevant info for the current state of Hanabi. The keys are:
-            "score": current game score
-            "deck_size": how many cards are left in the deck
-            "discarded": list of discarded cards
-            "disclosures": number of times information can be given
-            "mistakes_left": how many more mistakes can be made
-            "num_players": number of players in the game
-            "hands": for player_index, all visible and known cards (you can't see your own cards)
-            "known_info": all given information about each hand, so you can avoid telling the same info
-            "scored_cards": dictionary of highest scored card per pile. Uses first letter of a color as key (i.e. "R")
-            "history": list of moves objects made throughout the game. All are defined in hanabi_moves.py
+        @param game_info: object of all relevant info for the current state of Hanabi. The fields are:
+            score: current game score
+            deck_size: how many cards are left in the deck
+            discarded: list of discarded cards
+            disclosures: number of times information can be given
+            mistakes_left: how many more mistakes can be made
+            num_players: number of players in the game
+            hands: for player_index, all visible and known cards (you can't see your own cards)
+            known_info: all given information about each hand, so you can avoid telling the same info
+            scored_cards: dictionary of highest scored card per pile. Uses first letter of a color as key (i.e. "R")
+            history: list of moves objects made throughout the game. All are defined in hanabi_moves.py
 
         @return an Action of the command to do. The Actions are
             HanabiPlayAction: plays a card
