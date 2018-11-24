f7b69bc8c2adf2d77c3c29df05fa5521357522ee
Ari <ari.kaps@gmail.com>
2018-11-24 00:56:24 -0800

    Adding setup.py to project, and adjusting travis config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='hanabi_ai',
+      version='1.0',
+      description='Hanabi AI',
+      author='akaps',
+      author_email='ari.kaps@gmail.com',
+      url='https://github.com/akaps/hanabi_ai',
+      packages=['hanabi_ai'],
+     )
