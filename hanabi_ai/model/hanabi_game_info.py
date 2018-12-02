83be45678626743ebe864841bead77ae74943948
Ari <ari.kaps@gmail.com>
2018-12-02 12:51:17 -0800

    enable dangerous-default-value
    dlete dangerous-default-value from disabled list, fix messages
    default values are initalized once, so multiple calls
    with a dangerous default for example, fills an existing list
    this is different from initializing an empty list

--- hanabi_ai/model/hanabi_game_info.py
+++ hanabi_ai/model/hanabi_game_info.py
@@ -2,14 +2,14 @@ class GameInfo(object):
     def __init__(self,
                  score=0,
                  deck_size=0,
-                 discarded=[],
+                 discarded=None,
                  disclosures=0,
                  mistakes_left=0,
                  num_players=0,
-                 hands=[],
-                 known_info=[],
-                 scored_cards=[],
-                 history=[]):
+                 hands=None,
+                 known_info=None,
+                 scored_cards=None,
+                 history=None):
                  self.score = score
                  self.deck_size = deck_size
                  self.discarded = discarded
