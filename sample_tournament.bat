3a65276902873b9bc70e3c487a845e4b9144d3f2
Ari <ari.kaps@gmail.com>
2019-08-06 19:09:24 -0700

    Adding bat files for Windows
    Support running hanabi_ai from Windows

---
+++ sample_tournament.bat
@@ -0,0 +1,7 @@
+python hanabi_ai\play_game.py ^
+    "tournament" ^
+    "hanabi_ai.players.example_discarder.Discarder" ^
+    "hanabi_ai.players.example_discarder.Discarder" ^
+    "hanabi_ai.players.example_discarder.Discarder" ^
+    --log_dir "game_logs/tournament_results.txt" ^
+    --log_stderr "game_logs/tournament_errors.txt"
